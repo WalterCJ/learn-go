86fdf7523de2f6b06f748eeebc92a2718e6ffe28
WalterCJ <waltercavalcantejunior@gmail.com>
2019-09-01 21:19:26 -0300

    refactor: add new languages

--- hello.go
+++ hello.go
@@ -2,17 +2,34 @@ package main
 
 import "fmt"
 
-
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const french = "French"
+const spanish = "Spanish"
 
 // Hello retruns the strig "Hello, world"
-func Hello(name string) string {
-    if name == "" {
-        name = "World"
-    }
-    return englishHelloPrefix + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+//Return the right prefix depending on the language
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
